f8cacc967f0c04ea8d1767a4f81a25024e95f5df
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-02-19 22:24:37 +0700

    bai tap 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  const [date, setDate] = useState(new Date())
+  useEffect(() => {
+    console.log(date)
+      setTimeout(() =>{setDate( new Date())}, 5000)
+  },[date])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+ time:{date.toLocaleTimeString()}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
